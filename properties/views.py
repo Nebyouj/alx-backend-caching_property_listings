b5307d19b797405ac457a3483515201a9709f313
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-08-31 11:28:39 +0300

    Caching in Django

--- properties/views.py
+++ properties/views.py
@@ -14,3 +14,24 @@ from .utils import get_all_properties
 def property_list(request):
     properties = get_all_properties()
     return JsonResponse(properties, safe=False)
+
+
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
+
+@cache_page(60 * 15)  # 15 minutes caching
+def property_list(request):
+    properties = get_all_properties()
+    data = [
+        {
+            "id": prop.id,
+            "title": prop.title,
+            "description": prop.description,
+            "price": str(prop.price),
+            "location": prop.location,
+            "created_at": prop.created_at.isoformat(),
+        }
+        for prop in properties
+    ]
+    return JsonResponse({"data": data})
